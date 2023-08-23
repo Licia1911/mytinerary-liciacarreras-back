b82253871dd4423a9aa9b491af6683c39ee521d4
Licia <licia.carreras@gmail.com>
2023-08-23 05:55:10 -0300

    Modifico filtro

--- controller/cities/read.js
+++ controller/cities/read.js
@@ -1,47 +1,32 @@
 import City from "../../models/City.js";
 
-export default async (req,res,next) => {
+export default async (req, res) => {
     try {
-        console.log(req.query);   //QUERY ES UN OBJETO CON TODAS LAS CONSULTAS/IGUALDADES A BUSCAR EN LA BASE DE DATOS
-        //let objetoDeBusqueda = { admin_id:'64d641453319242989c30fce' } //para el ejemplo ES IGUAL a req.query
-        let objetoDeBusqueda = {}
-        let objetoDeOrdenamiento = {}
-        if (req.query.admin_id) {
-            objetoDeBusqueda.admin_id = req.query.admin_id
+        let busqueda = {};
+        let ordenamiento = {};
+
+        if (req.query.admin_) {
+            busqueda.admin_id = req.query.admin_id;
         }
         if (req.query.city) {
-            const cityFilter = req.query.city.trim(); // Elimina espacios en blanco al comienzo y al final
-            objetoDeBusqueda.city = { $regex: `^${cityFilter}`, $options: "i" };
-            // objetoDeBusqueda.city =req.query.city
-            //objetoDeBusqueda.city = { $regex: `^${req.query.city}`, $options: "i" };
-            // objetoDeBusqueda.city = new RegExp(req.query.city,'i')
-            //new RegExp(req.query.title,'i')+
+            busqueda.city = new RegExp('^' + req.query.city, 'i');
         }
         if (req.query.sort) {
-            objetoDeOrdenamiento.city = req.query.sort
-            //agrego la propiedad por la cual QUIERO ORDENAR
-            //si es 1 ordena ascendentemente
-            //si es -1 ordena descendentemente
+            ordenamiento.city = req.query.sort;
         }
-        let allCities = await City
-            .find(objetoDeBusqueda,'country city photo smalldescription admin_id')
-            .populate('admin_id','photo name mail -_id')
-            .sort(objetoDeOrdenamiento)
-        //let allCities = await City.find().select('country city photo smalldescription admin_id').populate('admin_id','photo name mail -_id')
-        if (allCities.length>0) {
-            return res.status(200).json({
-                success: true,
-                message: 'cities found',
-                response: allCities
-            })
-        } else {
-            return res.status(404).json({
-                success: false,
-                message: 'not found',
-                response: null
-            })
-        }
+        let allCities = await City.find(busqueda, 'country city photo smalldescription admin_id')
+            .populate('admin_id', 'photo name mail -_id')
+            .sort(ordenamiento)
+        return res.status(200).json({
+            success: true,
+            message: 'cities found',
+            response: allCities
+        })
     } catch (error) {
-        next(error)
+        return res.status(400).json({
+            success: false,
+            message: 'not found',
+            response: null
+        })
     }
 }
