f7e37f4d4f7fa421991abcb3285e6cffdae61c28
Licia <licia.carreras@gmail.com>
2023-09-08 17:16:48 -0300

    retoques

--- controller/auth/register.js
+++ controller/auth/register.js
@@ -1,11 +1,11 @@
 import User from "../../models/User.js"
 
-export default async(req,res,next) =>{
-
+export default async (req, res, next) => {
+    console.log(req.body)
     try {
         let one = await User.create(req.body)
         return res.status(201).json({
-            success:true,
+            success: true,
             message: 'user created',
             response: one._id
         })
