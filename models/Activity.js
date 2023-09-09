3cfc44aa0d0aac59f4d5b559a6294496365751ed
Licia <licia.carreras@gmail.com>
2023-09-09 18:37:08 -0300

    Modificaciones Activities

--- models/Activity.js
+++ models/Activity.js
@@ -3,10 +3,13 @@ import { model, Schema, Types } from "mongoose";
 let collection = 'activities'
 
 let schema = new Schema({
-    name: { type:String,required:true },
-    photo: { type:String, required:true},
-    itinerary_id: {type:Types.ObjectId, required:true, ref:'name'}
+    name: { type: String, required: true },
+    photo: { type: String, required: true },
+    itinerary_id: { type: Types.ObjectId, required: true, ref: 'itineraries' }
+}, {
+    timestamps: true
 })
+
 
 let Activity = model(collection, schema)
 
