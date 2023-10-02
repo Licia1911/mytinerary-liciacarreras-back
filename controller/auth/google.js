62322e88a658dcd4e959fdbfd8e3b81e53b7f1ee
Licia <licia.carreras@gmail.com>
2023-10-02 11:17:20 -0300

    Comments ok

--- controller/auth/google.js
+++ controller/auth/google.js
@@ -2,13 +2,17 @@ export default (req, res, next) => {
     try {
         return res.status(200).json({
             success: true,
-            message: 'user logged in with token',
+            message: 'user logged in with google',
             response: {             //TOKEN + DATOS DEL USUARIO
                 token: req.token,
                 user: req.user
             }
         })
     } catch (error) {
-        next(error)
+        res.status(500).json({
+            success: false,
+            message: 'User authentication failure',
+            error: error.message //este de acá
+        });
     }
 }
